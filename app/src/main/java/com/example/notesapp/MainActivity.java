a599cb61edbd51907151d1a577d6340e2d56cdc6
SandroSviben <sandro.sviben@agrivi.com>
2019-07-01 16:38:41 +0200

    Arhitektura za bazu podataka

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.notesapp;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DBOpenHelper helper = new DBOpenHelper(this);
+        SQLiteDatabase database = helper.getWritableDatabase();
     }
 }
